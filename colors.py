bf4424c134167ae25030844b243680c40ae70000
Alex561 <alexchapp@yahoo.com>
2017-05-10 12:41:04 -0700

    Minecraft wolls

---
+++ colors.py
@@ -0,0 +1,20 @@
+'''
+white 221, 221, 221
+orange 219, 125, 62
+magenta 179, 80, 188
+light blue 107, 138, 201
+yellow 177, 166, 39
+lime 65, 174, 56
+very pink rgb(208, 132, 153)
+grey rgb(64, 64, 64)
+light grey rgb(154, 161, 161)
+cyan rgb(46, 110, 137)
+purple rgb(126, 61, 181)
+blue rgb(46, 56, 141)
+brown rgb(79, 50, 31)
+green rgb(53, 70, 27)
+red rgb(150, 52, 48)
+black rgb(25, 22, 22)	
+'''
+colors = {'WHITE': (221,221,221), 'ORANGE':(219,125,62),'MAGENTA':(179,180,188),'lIGHT_BLUE':(107,138,201),'YELLOW': (177,166,39), 'LIME': (65,174,56),'PINK':(208,132,153),'GREY':(64,64,64),'SILVER': (154,161,161),
+       'CYAN': (46,110,137),'PURPLE':(126,61,181),'BLUE':(46, 56, 141),'BROWN': (79, 50, 31), 'GREEN': (53, 70, 27),'RED':(150, 52, 48),'BLACK':(25, 22, 22)}
