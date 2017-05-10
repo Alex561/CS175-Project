c227a6af76884269208fcf484fedd6e141d8bb8d
Alex561 <alexchapp@yahoo.com>
2017-05-10 14:16:32 -0700

    added more colors (partially)

--- colors.py
+++ colors.py
@@ -1,20 +1,39 @@
 '''
-white 221, 221, 221
-orange 219, 125, 62
-magenta 179, 80, 188
-light blue 107, 138, 201
-yellow 177, 166, 39
-lime 65, 174, 56
-very pink rgb(208, 132, 153)
-grey rgb(64, 64, 64)
-light grey rgb(154, 161, 161)
-cyan rgb(46, 110, 137)
-purple rgb(126, 61, 181)
-blue rgb(46, 56, 141)
-brown rgb(79, 50, 31)
-green rgb(53, 70, 27)
-red rgb(150, 52, 48)
-black rgb(25, 22, 22)	
+white (221, 221, 221)
+orange (219, 125, 62)
+magenta (179, 80, 188)
+light blue (107, 138, 201)
+yellow (177, 166, 39)
+lime (65, 174, 56)
+pink(208, 132, 153)
+grey (64, 64, 64)
+silver (154, 161, 161)
+cyan (46, 110, 137)
+purple (126, 61, 181)
+blue (46, 56, 141)
+brown (79, 50, 31)
+green (53, 70, 27)
+red (150, 52, 48)
+black (25, 22, 22)
+
+other blocks:
+pumpkin (227, 144, 29)
+netherrack (139, 81, 81)
+sponge (193, 193, 63)
+red_mushroom (181, 29,27)
+obsidian (38, 32, 53)
+gravel (135, 127, 127)
+gold_block (255, 245, 77)
+diamond_block (113, 225, 220)
+soul_sand (70, 51, 40)
+snow_layer / snow ??? (240, 252, 252) // this might need more fiddling with
+iron_block (235, 235, 235)
+clay (162, 168, 181)
+sand (228, 220, 170)
+sand_stone (219, 212, 162) // pretty much the same as sand
+wood (92, 73, 44)
+lapis_block (26, 70, 172)
+
 '''
 colors = {'WHITE': (221,221,221), 'ORANGE':(219,125,62),'MAGENTA':(179,180,188),'LIGHT_BLUE':(107,138,201),'YELLOW': (177,166,39), 'LIME': (65,174,56),'PINK':(208,132,153),'GRAY':(64,64,64),'SILVER': (154,161,161),
        'CYAN': (46,110,137),'PURPLE':(126,61,181),'BLUE':(46, 56, 141),'BROWN': (79, 50, 31), 'GREEN': (53, 70, 27),'RED':(150, 52, 48),'BLACK':(25, 22, 22)}
