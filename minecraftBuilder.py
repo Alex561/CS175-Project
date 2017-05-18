f77788da5432ae094ba5d799f327783f80846284
Edwin Ho <eaho555@gmail.com>
2017-05-18 12:24:10 -0700

    Fixed color dict

--- minecraftBuilder.py
+++ minecraftBuilder.py
@@ -72,8 +72,8 @@ def picturefy(pixelArray, woolDict):
     #         np.concatenate(newRow, getIfromRGB(pixelArray[y][x]))
     #
     #     np.concatenate(newPixelArray, newRow)
-    #
-    #
+
+
     # print(newPixelArray.shape)
     # print(newPixelArray[0,0])
 
@@ -83,6 +83,28 @@ def picturefy(pixelArray, woolDict):
     #
     # pixelArray = pca.transform(pixelArray)
 
+    # n_components = 2
+    # ipca = decomposition.IncrementalPCA(n_components=n_components, batch_size=10)
+    # X_ipca = ipca.fit_transform(pixelArray)
+    # pca = decomposition.(n_components=n_components)
+    # X_pca = pca.fit_transform(pixelArray)
+    # colors = ['navy', 'turquoise', 'darkorange']
+    # for X_transformed, title in [(X_ipca, "Incremental PCA"), (X_pca, "PCA")]:
+    #     plt.figure(figsize=(8, 8))
+    #     for color, i, target_name in zip(colors, [0, 1, 2], pixelArray.target_names):
+    #         plt.scatter(X_transformed[y == i, 0], X_transformed[y == i, 1],
+    #                     color=color, lw=2, label=target_name)
+    #         if "Incremental" in title:
+    #             err = np.abs(np.abs(X_pca) - np.abs(X_ipca)).mean()
+    #             plt.title(title + " of iris dataset\nMean absolute unsigned error "
+    #                               "%.6f" % err)
+    #     else:
+    #         plt.title(title + " of iris dataset")
+    #     plt.legend(loc="best", shadow=False, scatterpoints=1)
+    #     plt.axis([-4, 4, -1.5, 1.5])
+    #     plt.show()
+
+
     xCount = 0
     yCount = 0
     for x in range(pixelArray.shape[1]):
@@ -91,12 +113,16 @@ def picturefy(pixelArray, woolDict):
             yCount += 1
             if (isAlpha and pixelArray[y][x][3] > 0):
                 closestWool = closestColor(pixelArray[y][x], woolDict)
-                returnString += '<DrawBlock x="{0}" y="{1}" z="10" type="wool" colour="{2}"/>\n'.format(x, y+7, closestWool)
+                # returnString += '<DrawBlock x="{0}" y="{1}" z="10" type="lapis_block"/>\n'.format(x, y+7)
+                if closestWool in woolColors.wool:
+                    returnString += '<DrawBlock x="{0}" y="{1}" z="10" type="wool" colour="{2}"/>\n'.format(x, y+7, closestWool)
+                else:
+                    returnString += '<DrawBlock x="{0}" y="{1}" z="10" type="{2}"/>\n'.format(x, y+7, closestWool)
 
     print("({0}, {1}) pixels", xCount, yCount/xCount)
     return returnString
 
-imageFile = "Overwatch.png"
+imageFile = "Nyan.png"
 image = io.imread(imageFile)
 
 sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)  # flush print output immediately
