7c679364fedb6216094a4a2981be71205cd46a72
Alex561 <alexchapp@yahoo.com>
2017-05-18 12:34:12 -0700

    PCA function

--- minecraftBuilder.py
+++ minecraftBuilder.py
@@ -29,6 +29,37 @@ import colors as woolColors
 from skimage import io
 from sklearn import decomposition
 
+def pca(X):
+  # Principal Component Analysis
+  # input: X, matrix with training data as flattened arrays in rows
+  # return: projection matrix (with important dimensions first),
+  # variance and mean
+
+  #shape the rgb bands
+  X = X.reshape(-1, 3)
+  #get dimensions
+  num_data,dim = X.shape
+
+  #center data
+  mean_X = X.mean(axis=0)
+  for i in range(num_data):
+      X[i] -= mean_X
+
+  if dim>100:
+      print 'PCA - compact trick used'
+      M = dot(X,X.T) #covariance matrix
+      e,EV = linalg.eigh(M) #eigenvalues and eigenvectors
+      tmp = dot(X.T,EV).T #this is the compact trick
+      V = tmp[::-1] #reverse since last eigenvectors are the ones we want
+      S = sqrt(e)[::-1] #reverse since eigenvalues are in increasing order
+  else:
+      print 'PCA - SVD used'
+      U,S,V = linalg.svd(X)
+      V = V[:num_data] #only makes sense to return the first num_data
+
+   #return the projection matrix, the variance and the mean
+   return V,S,mean_X
+
 def getRGBfromI(RGBint):
     blue = RGBint & 255
     green = (RGBint >> 8) & 255
