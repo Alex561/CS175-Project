45a02d6d0aef3947489a628bd9c5d5a8d87448bd
Alex561 <alexchapp@yahoo.com>
2017-05-24 15:24:35 -0700

    gui integration

--- minecraftBuilder.py
+++ minecraftBuilder.py
@@ -18,7 +18,7 @@
 # ------------------------------------------------------------------------------------------------
 
 # Tutorial sample #2: Run simple mission using raw XML
-
+from Tkinter import *
 import numpy as np
 import MalmoPython
 import os
@@ -88,6 +88,18 @@ import Image
 
 difList = []
 
+#get resolution button
+def getResolution():
+    userInputP.get()
+    resolution =0
+    resolutionLabel=Label(root,text="Resolution: "+str(resolution))
+    resolutionLabel.pack()
+    resolutionLabel.place(x=150,y=60)
+#drawPciture button
+def drawPicture():
+   userInputH.get()
+   userInputW.get()
+   userInputP.get()
 def QuantitiveEval(difList):
     sum=0
     for i in range(len(difList)):
@@ -140,7 +152,38 @@ def picturefy(pixelArray, woolDict):
     print("({0}, {1}) pixels", xCount, yCount/xCount)
     print(QuantitiveEval(difList))
     return returnString
+#initilize gui
+root = Tk()
+frame = Frame(root, width=300, height=300)
+frame.pack()
+userInputH = StringVar()#Height Stuff
+heightLabel=Label(root,text="Height")
+heightLabel.pack()
+heightLabel.place(x=20,y=150)
+height = Entry(root, text="Black", fg="black",textvariable=userInputH)
+height.place(x=60,y=150)
 
+userInputW = StringVar()#width
+widthLabel=Label(root,text="Width")
+widthLabel.place(x=20,y=100)
+width = Entry(root, text="Black", fg="black",textvariable=userInputW)
+width.place(x=60,y=100)
+
+userInputP = StringVar()#picture box
+pictureLabel=Label(root,text="Picture File")
+pictureLabel.place(x=20,y=20)
+picture = Entry(root, text="Black", fg="black",textvariable=userInputP)
+picture.place(x=90,y=20)
+
+getResolution= Button(root, text ="Get Resolution", command = getResolution)#resolution button
+getResolution.pack()
+getResolution.place(x=30,y=60)
+
+drawPicture = Button(root, text ="Draw picture", command = drawPicture)#draw button
+drawPicture.pack()
+drawPicture.place(x=75,y=200)
+
+#picture stuff
 imageFile = "Tina.jpg"
 image = io.imread(imageFile)
 image = resize(image, (200, 200))
