2a2f12122d08f6a8227948124be85494d16ca7ba
Edwin Ho <eaho555@gmail.com>
2017-05-31 14:12:11 -0700

    background for depth

--- colors.py
+++ colors.py
@@ -50,9 +50,9 @@ noteblock 114, 73, 51
 
 '''
 
-wool = ['WHITE', 'ORANGE', 'MAGENTA', 'LIGHT_BLUE', 'YELLOW', 'LIME', 'PINK', 'GRAY', 'SILVER', 'CYAN', 'PURPLE', 'BLUE', 'BROWN', 'RED', 'BLACK', 'GREEN']
+woolList = ['WHITE', 'ORANGE', 'MAGENTA', 'LIGHT_BLUE', 'YELLOW', 'LIME', 'PINK', 'GRAY', 'SILVER', 'CYAN', 'PURPLE', 'BLUE', 'BROWN', 'RED', 'BLACK', 'GREEN']
 
-colors = {'WHITE': (221,221,221), 'ORANGE':(219,125,62),'MAGENTA':(179,80,188),'LIGHT_BLUE':(107,138,201),'YELLOW': (177,166,39), 'LIME': (65,174,56),'PINK':(208,132,153),'GRAY':(64,64,64),'SILVER': (154,161,161),
+colorDict = {'WHITE': (221,221,221), 'ORANGE':(219,125,62),'MAGENTA':(179,80,188),'LIGHT_BLUE':(107,138,201),'YELLOW': (177,166,39), 'LIME': (65,174,56),'PINK':(208,132,153),'GRAY':(64,64,64),'SILVER': (154,161,161),
        'CYAN': (46,110,137),'PURPLE':(126,61,181),'BLUE':(46, 56, 141),'BROWN': (79, 50, 31), 'GREEN': (53, 70, 27),'RED':(150, 52, 48),'BLACK':(25, 22, 22),'netherrack': (139, 81, 81),
         'sponge': (193, 193, 63),'red_mushroom_block': (181, 29,27),'obsidian': (38, 32, 53),'gold_block': (255, 245, 77),'diamond_block': (113, 225, 220),'soul_sand': (70, 51, 40),
         'snow':(240, 252, 252),'iron_block': (235, 235, 235),'clay':(162, 168, 181),'sandstone': (219, 212, 162),'lapis_block': (26, 70, 172), 'stone': (120, 120, 120),
--- minecraftBuilder.py
+++ minecraftBuilder.py
@@ -21,110 +21,43 @@
 from Tkinter import *
 import numpy as np
 import MalmoPython
-import os
 import sys
 import time
 
-import colors as woolColors
+import colors
 from skimage import io
 from sklearn import *
 import cv2
-# from sklearn import decomposition
-
-# def pca(X, yDim):
-#     # Principal Component Analysis
-#     # input: X, matrix with training data as flattened arrays in rows
-#     # return: projection matrix (with important dimensions first),
-#     # variance and mean
-#
-#     #shape the rgb bands
-#     X = flattenColorImage(X)
-#
-#     svd = decomposition.TruncatedSVD(n_components=yDim, algorithm="arpack")
-#
-#     svd.fit(X)
-#
-#     newX = svd.transform(X).astype(int)
-#
-#     print(newX.shape)
-#     print(newX[0,0])
-#
-#     # return unflattenColorImage(newX) - broken atm, was working on this
-#     return newX
-
-# def flattenColorImage(colorImage):
-#     flattended = np.zeros(shape=(colorImage.shape[0], colorImage.shape[1]))
-#     for y in range(colorImage.shape[0]):
-#         newRow = np.zeros(colorImage.shape[1])
-#         for x in range(colorImage.shape[1]):
-#             newRow[x] = getIfromRGB(colorImage[y][x])
-#         flattended[y] = newRow
-#
-#     return flattended
-#
-# def unflattenColorImage(flattenedImage):
-#     unflattended = np.zeros(shape=(flattenedImage.shape[0], flattenedImage.shape[1], 3))
-#     for y in range(flattenedImage.shape[0]):
-#         newRow = np.zeros(flattenedImage.shape[1])
-#         for x in range(flattenedImage.shape[1]):
-#             rgbFromI = getRGBfromI(flattenColorImage[y][x])
-#             newRow[y][x] = rgbFromI
-#         unflattended[y] = newRow
-#
-#     return unflattended
-#
-# def getRGBfromI(RGBint):
-#     blue = RGBint & 255
-#     green = (RGBint >> 8) & 255
-#     red = (RGBint >> 16) & 255
-#     return red, green, blue
-#
-# def getIfromRGB(rgb):
-#     red = rgb[0]
-#     green = rgb[1]
-#     blue = rgb[2]
-#     RGBint = (red << 16) + (green << 8) + blue
-#     return RGBint
 
 difList = []
 
 def main(imageString, imageHeight, imageWidth):
     difList = []
 
-    # picture stuff
-
-    # image = io.imread(imageString)
-    # image = resize(image, (imageHeight, imageWidth))
     image = cv2.imread(imageString)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     image = np.rot90(image, k=2)
 
     gray_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-    # gb_kernel = cv2.getGaborKernel((31, 31), 4.0, np.pi, 10.0, 0.5, 0, ktype=cv2.CV_32F)
-    # img_filtered = cv2.filter2D(gray, cv2.CV_8U, gb_kernel.transpose())
-    depth_map = cv2.distanceTransform(gray_image, cv2.cv.CV_DIST_L2, cv2.cv.CV_DIST_MASK_PRECISE)
-    # depth_map = preprocessing.normalize(depth_map)
+
+    # depth_map = cv2.distanceTransform(gray_image, cv2.cv.CV_DIST_L2, cv2.cv.CV_DIST_MASK_PRECISE)
+    # depth_map = cv2.distanceTransform(gray_image, cv2.cv.CV_DIST_L1, 3)
+    depth_map = cv2.distanceTransform(gray_image, cv2.cv.CV_DIST_C, 5)
+
     depth_map = preprocessing.scale(depth_map)
 
-    image = cv2.resize(image, (imageHeight, imageWidth))
-    depth_map = cv2.resize(depth_map, (imageHeight, imageWidth))
-    # print(depth_map.shape)
+    image = cv2.resize(image, (imageWidth, imageHeight))
+    depth_map = cv2.resize(depth_map, (imageWidth, imageHeight))
+
     print(depth_map.min())
     print(depth_map.max())
     print(depth_map.mean())
     print(depth_map)
 
-    # thrs = cv2.getTrackbarPos('threshold', 'distrans')
-    # mark = cv2.Canny(image, thrs, 3 * thrs)
-    # dist, labels = cv2.distanceTransformWithLabels(~mark, cv2.DIST_L2, 5)
-
     import matplotlib.pyplot as plt
-    plt.imshow(image)
+    plt.imshow(np.rot90(image, k=-2))
+    plt.imshow(np.rot90(depth_map, k=-2))
     plt.show()
-
-    # sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)  # flush print output immediately
-
-    # More interesting generator string: "3;7,44*49,73,35:1,159:4,95:13,35:13,159:11,95:10,159:14,159:6,35:6,95:6;12;"
 
     missionXML = '''<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
                 <Mission xmlns="http://ProjectMalmo.microsoft.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
@@ -137,15 +70,15 @@ def main(imageString, imageHeight, imageWidth):
                     <ServerHandlers>
                       <FlatWorldGenerator generatorString="3;7,0,5*3,2;3;,biome_1" forceReset="true"/>
                       <DrawingDecorator>
-                        ''' + picturefy(image, depth_map, woolColors.colors) + '''
+                        ''' + picturefy(image, depth_map, colors.colorDict) + '''
                       </DrawingDecorator>
                       <ServerQuitFromTimeUp timeLimitMs="1000"/>
                       <ServerQuitWhenAnyAgentFinishes/>
                     </ServerHandlers>
                   </ServerSection>
 
-                  <AgentSection mode="Survival">
-                    <Name>MalmoTutorialBot</Name>
+                  <AgentSection mode="Creative">
+                    <Name>SketchyAI</Name>
                     <AgentStart>
                         ''' + '<Placement x="{0}" y="{1}" z="{2}" yaw="0"/>'.format(imageWidth/2, imageHeight/2, -imageHeight * .75) + '''
                     </AgentStart>
@@ -219,10 +152,10 @@ def QuantitiveEval(difList):
         sum+=difList[i]
     return sum/len(difList)
 
-def closestColor(pixel, woolDict):
+def closestColor(pixel, colorDict):
     resultColor = "WHITE"
     minDif = float("inf")
-    for wool, colorValue in woolDict.items():
+    for wool, colorValue in colorDict.items():
         dif = np.sqrt((pixel[0]-colorValue[0])**2 + (pixel[1]-colorValue[1])**2 + (pixel[2]-colorValue[2])**2)
         if (dif < minDif):
             resultColor = wool
@@ -231,7 +164,7 @@ def closestColor(pixel, woolDict):
     difList.append(minDif)
     return resultColor
 
-def picturefy(pixelArray, depth_map, woolDict):
+def picturefy(pixelArray, depth_map, colorDict):
     returnString = ""
     isAlpha = False
     if (pixelArray.shape[2] == 4):
@@ -241,28 +174,35 @@ def picturefy(pixelArray, depth_map, woolDict):
 
     xCount = 0
     yCount = 0
+    layerCount = 10
+    zScale = 3
+
     for x in range(pixelArray.shape[1]):
         xCount += 1
+
         for y in range(pixelArray.shape[0]):
             yCount += 1
 
-            # pixelColor = pixelArray[y][x] * 255
             pixelColor = pixelArray[y][x]
             depth = depth_map[y][x]
+
             if (isAlpha):
                 pixelColor = round(pixelColor[0]), round(pixelColor[1]), round(pixelColor[2]), round(pixelColor[3])
             else:
                 pixelColor = round(pixelColor[0]), round(pixelColor[1]), round(pixelColor[2])
 
-            # print(pixelColor)
             if (isAlpha and pixelArray[y][x][3] > 0) or (not isAlpha):
-                closestWool = closestColor(pixelColor, woolDict)
-                # zValue = (depth * depth_map.max()) + (1 - depth) * depth_map.min();
-                zValue = int(round((depth - depth_map.min()) / (depthRange) * 10))
-                if closestWool in woolColors.wool:
-                    returnString += '<DrawBlock x="{0}" y="{1}" z="{2}" type="wool" colour="{3}"/>\n'.format(x, y+7, zValue, closestWool)
+                bestColor = closestColor(pixelColor, colorDict)
+                zValue = int(round((depth - depth_map.min()) / (depthRange) * layerCount)) * zScale
+                if bestColor in colors.woolList:
+                    returnString += '<DrawBlock x="{0}" y="{1}" z="{2}" type="wool" colour="{3}"/>\n'.format(x, y+7, zValue, bestColor)
                 else:
-                    returnString += '<DrawBlock x="{0}" y="{1}" z="{2}" type="{3}"/>\n'.format(x, y+7, zValue, closestWool)
+                    returnString += '<DrawBlock x="{0}" y="{1}" z="{2}" type="{3}"/>\n'.format(x, y+7, zValue, bestColor)
+
+    #Draw gray background
+    for x in range(-50, pixelArray.shape[1] + 50):
+        for y in range(pixelArray.shape[0] + 10):
+            returnString += '<DrawBlock x="{0}" y="{1}" z="{2}" type="wool" colour="GRAY"/>\n'.format(x, y + 7, 10 * zScale)
 
     print("({0}, {1}) pixels".format(xCount, yCount/xCount))
     print(QuantitiveEval(difList))
